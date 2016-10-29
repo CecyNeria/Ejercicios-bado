89bd852f86847078fc2730de6f926666498d3b3d
Imac <cecyholmes221@gmail.com>
2016-10-29 12:57:13 -0500

    lo que se supone que hice

---
+++ functions.js
@@ -0,0 +1,12 @@
+var computadora =  Math.floor((Math.random()*3)+1); 
+var respuesta;
+var jugador1 = prompt ("que escoges piedra, papel o tijera")
+
+
+
+//elección de la computadora
+var aleatorio = function() {
+    var numero =  Math.floor((Math.random()*3)+1); 
+    var respuesta;
+    if (computadora=1
+    	) {respuesta=tieras }
